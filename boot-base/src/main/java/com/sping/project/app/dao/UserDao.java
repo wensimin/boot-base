ce28f9e6414b7b902536c0952d842f423e903b58
shali <hukaizhiyu2@gmail.com>
2017-07-14 09:57:05 +0800

    新增predicate查询示例

--- boot-base/src/main/java/com/sping/project/app/dao/UserDao.java
+++ boot-base/src/main/java/com/sping/project/app/dao/UserDao.java
@@ -3,9 +3,10 @@ package com.sping.project.app.dao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.sping.project.app.entity.User;
 
-public interface UserDao extends JpaRepository<User, Long> {
+public interface UserDao extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 	Page<User> findByName(String name, Pageable pageable);
 }
--- boot-base/src/main/java/com/sping/project/app/service/TestService.java
+++ boot-base/src/main/java/com/sping/project/app/service/TestService.java
@@ -1,12 +1,16 @@
 package com.sping.project.app.service;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.criteria.Predicate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.sping.project.app.dao.UserDao;
 import com.sping.project.app.entity.User;
@@ -28,8 +32,17 @@ public class TestService {
 		if (page.getQuery() == null) {
 			return userDao.findAll(page.getPageRequest());
 		} else {
-			//DEMO mapper test
+			// mybatis query
 			userMapper.findByNameLike(page.getQuery().getName());
+			// predicate query
+			userDao.findAll((root, query, cb) -> {
+				List<Predicate> list = new ArrayList<Predicate>();
+				if (!StringUtils.isEmpty(page.getQuery().getName())) {
+					list.add(cb.like(root.get("name").as(String.class), "%" + page.getQuery().getName() + "%"));
+				}
+				return query.where(cb.and(list.toArray(new Predicate[list.size()]))).getRestriction();
+			});
+			// QBE query
 			ExampleMatcher matcher = ExampleMatcher.matching().withMatcher("name", match -> match.contains());
 			return userDao.findAll(Example.of(page.getQuery(), matcher), page.getPageRequest());
 		}
