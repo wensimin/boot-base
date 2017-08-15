f3fdecc4fbc839104231f017ba99bd2818814969
shali <hukaizhiyu2@gmail.com>
2017-08-15 10:09:46 +0800

    增加cors支持

--- boot-base/src/main/java/tech/shali/project/app/WebAppConfigurer.java
+++ boot-base/src/main/java/tech/shali/project/app/WebAppConfigurer.java
@@ -1,7 +1,9 @@
 package tech.shali.project.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -9,6 +11,8 @@ import tech.shali.project.app.web.security.SecurityInterceptor;
 
 @Configuration
 public class WebAppConfigurer extends WebMvcConfigurerAdapter {
+	@Value("${cors.control.maxAge}")
+	private int maxAge;
 	@Autowired
 	private SecurityInterceptor securityInterceptor;
 	@Override
@@ -16,4 +20,14 @@ public class WebAppConfigurer extends WebMvcConfigurerAdapter {
 		registry.addInterceptor(securityInterceptor);
 		super.addInterceptors(registry);
 	}
+	// @formatter:off
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedHeaders("*")
+		.allowedMethods("*")
+		.allowedOrigins("*")
+		.maxAge(maxAge);
+	}
+	// @formatter:on
 }
