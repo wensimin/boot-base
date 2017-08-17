b0c430a7b65cb8f4b429a5db72540384e7b21e9a
shali <hukaizhiyu2@gmail.com>
2017-08-17 10:23:47 +0800

    新增XSS防御

--- boot-base/src/main/java/tech/shali/project/app/WebAppConfigurer.java
+++ boot-base/src/main/java/tech/shali/project/app/WebAppConfigurer.java
@@ -2,12 +2,20 @@ package tech.shali.project.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
 import tech.shali.project.app.web.security.SecurityInterceptor;
+import tech.shali.project.app.web.security.xss.XSSFilter;
+import tech.shali.project.app.web.security.xss.XssStringJsonSerializer;
 
 @Configuration
 public class WebAppConfigurer extends WebMvcConfigurerAdapter {
@@ -19,6 +27,36 @@ public class WebAppConfigurer extends WebMvcConfigurerAdapter {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(securityInterceptor);
 		super.addInterceptors(registry);
+	}
+	
+	/**
+	 * xss过滤器 过滤xss值
+	 * 
+	 * @return bean
+	 */
+	@Bean
+	public XSSFilter xssFilter() {
+		return new XSSFilter();
+	}
+
+	/**
+	 * 描述 : xssObjectMapper
+	 *
+	 * @param builder
+	 *            builder
+	 * @return xssObjectMapper
+	 */
+	@Bean
+	@Primary
+	public ObjectMapper xssObjectMapper(Jackson2ObjectMapperBuilder builder) {
+		// 解析器
+		ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+		// 注册xss解析器
+		SimpleModule xssModule = new SimpleModule("XssStringJsonSerializer");
+		xssModule.addSerializer(new XssStringJsonSerializer());
+		objectMapper.registerModule(xssModule);
+		// 返回
+		return objectMapper;
 	}
 	// @formatter:off
 	@Override
