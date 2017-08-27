8a47436529af953f9a2f22cdc869a14770a4faba
wensimin <wensimin@wensimin-pc>
2017-08-27 14:52:16 +0800

    object send

--- boot-base/src/main/java/tech/shali/project/app/mq/Sender.java
+++ boot-base/src/main/java/tech/shali/project/app/mq/Sender.java
@@ -5,19 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import tech.shali.project.app.entity.User;
-import tech.shali.project.app.entity.eunm.UserType;
 
 @Component
 public class Sender {
 	@Autowired
 	private AmqpTemplate rabbitTemplate;
 
-	public void send() {
-		User user = new User();
-		user.beforeInsert();
-		user.setId(1L);
-		user.setName("test");
-		user.setType(UserType.a);
+	public void send(User user) {
 		System.out.println("Sender : " + user);
 		this.rabbitTemplate.convertAndSend("hello", user);
 	}
--- boot-base/src/test/java/tech/shali/project/app/test/HelloApplicationTests.java
+++ boot-base/src/test/java/tech/shali/project/app/test/HelloApplicationTests.java
@@ -1,19 +1,34 @@
 package tech.shali.project.app.test;
 
+import javax.transaction.Transactional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import tech.shali.project.app.entity.User;
+import tech.shali.project.app.entity.eunm.UserType;
 import tech.shali.project.app.mq.Sender;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-public class HelloApplicationTests {
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Transactional
+public class HelloApplicationTests {	
 	@Autowired
 	private Sender sender;
 
 	@Test
 	public void hello() throws Exception {
-		sender.send();
+		User user = new User();
+		user.beforeInsert();
+		user.setId(1L);
+		user.setName("test");
+		user.setType(UserType.a);
+		sender.send(user);
 	}
 }
