77ceed3316bdcf508f4f8645b1297a06a3df9978
shali <hukaizhiyu2@gmail.com>
2017-08-28 17:46:39 +0800

    拼写错误fix log替换

--- boot-base/src/main/java/tech/shali/project/app/mq/Receiver.java
+++ boot-base/src/main/java/tech/shali/project/app/mq/Receiver.java
@@ -1,5 +1,7 @@
 package tech.shali.project.app.mq;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -15,8 +17,10 @@ import tech.shali.project.app.entity.User;
 @Component
 @RabbitListener(queues = "${spring.rabbitmq.queue.name}")
 public class Receiver {
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
 	@RabbitHandler
 	public void process(User user) {
-		System.out.println("Receiver : " + user);
+		logger.debug("Receiver : " + user);
 	}
 }
--- boot-base/src/main/java/tech/shali/project/app/mq/Sender.java
+++ boot-base/src/main/java/tech/shali/project/app/mq/Sender.java
@@ -1,5 +1,7 @@
 package tech.shali.project.app.mq;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -10,9 +12,10 @@ import tech.shali.project.app.entity.User;
 public class Sender {
 	@Autowired
 	private AmqpTemplate rabbitTemplate;
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	public void send(User user) {
-		System.out.println("Sender : " + user);
+		logger.debug("Sender : " + user);
 		this.rabbitTemplate.convertAndSend("hello", user);
 	}
 }
