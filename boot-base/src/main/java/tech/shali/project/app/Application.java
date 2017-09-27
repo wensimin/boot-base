1718fae413cdf3c18f88ad6b13a807f3be41c13e
wensimin <hukaizhiyu2@gmail.com>
2017-09-27 22:45:37 +0800

    成功启动客户端

--- boot-base/src/main/java/tech/shali/project/app/Application.java
+++ boot-base/src/main/java/tech/shali/project/app/Application.java
@@ -2,18 +2,26 @@ package tech.shali.project.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 /**
  *  启动类
  * @author wensimin
  *
- * @SpringBootApplication
- * 		基础配置
- * @EnableResourceServer
- * 		开启资源服务器，这样只有OAuth2通过的才能进入
+ * @SpringBootApplication 基础配置
+ *
+ * @EnableResourceServer 开启资源服务器，这样只有OAuth2通过的才能进入
+ *
+ * @EnableEurekaClient 尤里卡客户端
+ *
  */
 @SpringBootApplication
 @EnableResourceServer
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
