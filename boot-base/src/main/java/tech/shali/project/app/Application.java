12263a174fcd58fc3fb37032f788a27a8a21768c
wensimin <hukaizhiyu2@gmail.com>
2017-11-04 14:06:23 +0800

    删除无用注解

--- boot-base/src/main/java/tech/shali/project/app/Application.java
+++ boot-base/src/main/java/tech/shali/project/app/Application.java
@@ -20,8 +20,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @SpringBootApplication
 @EnableResourceServer
 @EnableEurekaClient
-@EnableDiscoveryClient
-@EnableFeignClients
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
