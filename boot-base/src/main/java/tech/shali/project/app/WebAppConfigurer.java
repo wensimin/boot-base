431fa70a106485fff6213ec42fca0be3fb24c14c
wensimin <hukaizhiyu2@gmail.com>
2017-12-05 01:07:25 +0800

    copy

--- boot-base/src/main/java/tech/shali/project/app/WebAppConfigurer.java
+++ boot-base/src/main/java/tech/shali/project/app/WebAppConfigurer.java
@@ -1,5 +1,7 @@
 package tech.shali.project.app;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,10 +9,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-
 import tech.shali.project.app.web.security.xss.XssStringJsonSerializer;
 
 @Configuration
